cfe915c3add16bf62a1891160613b416c2018f69
Ahd <dafuq.ahd@gmail.com>
2020-05-14 15:01:33 +0300

    add three dropdown fields inputs

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  String locationValue = 'Location';
+  String typeValue = 'Type';
+  String priceValue = 'Max Price';
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -52,7 +61,7 @@ class Home extends StatelessWidget {
                         BorderRadius.only(topLeft: Radius.circular(150)),
                     color: const Color(0xff2980b9),
                   ),
-                  child: Row(
+                  child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
                       Padding(
@@ -60,6 +69,110 @@ class Home extends StatelessWidget {
                         child: Text(
                           "I'm looking for : ",
                           style: TextStyle(color: Colors.white, fontSize: 25),
+                        ),
+                      ),
+                      Container(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Container(
+                              child: DropdownButton<String>(
+                                value: locationValue,
+                                icon: Icon(Icons.arrow_downward,
+                                    color: Colors.white),
+                                iconSize: 24,
+                                elevation: 16,
+                                style: TextStyle(color: Colors.white),
+                                underline: Container(
+                                  height: 2,
+                                  color: Colors.white,
+                                ),
+                                onChanged: (String newValue) {
+                                  setState(() {
+                                    locationValue = newValue;
+                                  });
+                                },
+                                items: <String>[
+                                  'Location',
+                                  'Antalia',
+                                  'Bursa',
+                                  'Fethiye',
+                                  'Istanbul',
+                                ].map<DropdownMenuItem<String>>((String value) {
+                                  return DropdownMenuItem<String>(
+                                    value: value,
+                                    child: Text(value),
+                                  );
+                                }).toList(),
+                              ),
+                            ),
+                            Container(
+                              child: DropdownButton<String>(
+                                value: typeValue,
+                                icon: Icon(Icons.arrow_downward,
+                                    color: Colors.white),
+                                iconSize: 24,
+                                elevation: 16,
+                                style: TextStyle(color: Colors.white),
+                                underline: Container(
+                                  height: 2,
+                                  color: Colors.white,
+                                ),
+                                onChanged: (String newValue) {
+                                  setState(() {
+                                    typeValue = newValue;
+                                  });
+                                },
+                                items: <String>[
+                                  'Type',
+                                  'Apartment',
+                                  'Penthouse',
+                                  'Villa',
+                                ].map<DropdownMenuItem<String>>((String value) {
+                                  return DropdownMenuItem<String>(
+                                    value: value,
+                                    child: Text(value),
+                                  );
+                                }).toList(),
+                              ),
+                            ),
+                            Container(
+                              child: DropdownButton<String>(
+                                value: priceValue,
+                                icon: Icon(
+                                  Icons.arrow_downward,
+                                  color: Colors.white,
+                                ),
+                                iconSize: 24,
+                                elevation: 16,
+                                style: TextStyle(color: Colors.white),
+                                underline: Container(
+                                  height: 2,
+                                  color: Colors.white,
+                                ),
+                                onChanged: (String newValue) {
+                                  setState(() {
+                                    priceValue = newValue;
+                                  });
+                                },
+                                items: <String>[
+                                  'Max Price',
+                                  '50000',
+                                  '100000',
+                                  '150000',
+                                  '200000',
+                                  '300000',
+                                  '400000',
+                                  '500000',
+                                ].map<DropdownMenuItem<String>>((String value) {
+                                  return DropdownMenuItem<String>(
+                                    value: value,
+                                    child: Text(value),
+                                  );
+                                }).toList(),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     ],
