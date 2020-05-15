d39d99158a376e99c0a6eefbeb06916b0f5eae68
Ahd <dafuq.ahd@gmail.com>
2020-05-15 14:14:08 +0300

    create the form in single page and style it

--- lib/pages/single_page.dart
+++ lib/pages/single_page.dart
@@ -1,5 +1,6 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SinglePage extends StatefulWidget {
   final Map propertyData;
@@ -10,13 +11,6 @@ class SinglePage extends StatefulWidget {
 }
 
 class _SinglePageState extends State<SinglePage> {
-  bool expandImage;
-  @override
-  void initState() {
-    super.initState();
-    expandImage = false;
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,42 +28,281 @@ class _SinglePageState extends State<SinglePage> {
         ),
       ),
       body: Container(
-          child: Column(
-        children: <Widget>[
-          CarouselSlider(
-            options: CarouselOptions(
-              autoPlay: true,
-              enlargeCenterPage: true,
-              height: 300,
+        child: ListView(
+          children: <Widget>[
+            CarouselSlider(
+              options: CarouselOptions(
+                autoPlay: true,
+                enlargeCenterPage: true,
+                height: 300,
+              ),
+              items: [
+                widget.propertyData['acf']['image_one'],
+                widget.propertyData['acf']['image_two'],
+                widget.propertyData['acf']['image_three']
+              ]
+                  .map(
+                    (item) => Container(
+                      child: Container(
+                        margin: EdgeInsets.all(5.0),
+                        child: ClipRRect(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(5.0)),
+                            child: Stack(
+                              children: <Widget>[
+                                Image.network(
+                                  item,
+                                  fit: BoxFit.cover,
+                                  width: 1000.0,
+                                  height: 500,
+                                ),
+                              ],
+                            )),
+                      ),
+                    ),
+                  )
+                  .toList(),
             ),
-            items: [
-              widget.propertyData['acf']['image_one'],
-              widget.propertyData['acf']['image_two'],
-              widget.propertyData['acf']['image_three']
-            ]
-                .map(
-                  (item) => Container(
-                    child: Container(
-                      margin: EdgeInsets.all(5.0),
-                      child: ClipRRect(
-                          borderRadius: BorderRadius.all(Radius.circular(5.0)),
-                          child: Stack(
-                            children: <Widget>[
-                              Image.network(
-                                item,
-                                fit: BoxFit.cover,
-                                width: 1000.0,
-                                height: 500,
+            Card(
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text(
+                          widget.propertyData['title']['rendered'],
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: const Color(0xff6C54FF),
+                          ),
+                        ),
+                        Text(
+                          "${widget.propertyData['acf']['base_currency']} ${widget.propertyData['acf']['price']}",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: const Color(0xff6C54FF),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(
+                          FontAwesomeIcons.mapMarkerAlt,
+                          size: 15,
+                          color: Colors.blueGrey[600],
+                        ),
+                        Text(
+                          ' ${widget.propertyData['acf']['location']}',
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blueGrey[600],
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(
+                              FontAwesomeIcons.ruler,
+                              size: 17,
+                              color: Colors.blueGrey[600],
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                              child: Text(
+                                '${widget.propertyData['acf']['size']} ft/sq',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.blueGrey[600],
+                                ),
                               ),
-                            ],
-                          )),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(
+                              FontAwesomeIcons.bed,
+                              size: 17,
+                              color: Colors.blueGrey[600],
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                              child: Text(
+                                widget.propertyData['acf']['bedrooms'],
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.blueGrey[600],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(
+                              FontAwesomeIcons.bath,
+                              size: 17,
+                              color: Colors.blueGrey[600],
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                              child: Text(
+                                widget.propertyData['acf']['bathrooms'],
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.blueGrey[600],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(
+                              FontAwesomeIcons.home,
+                              size: 17,
+                              color: Colors.blueGrey[600],
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                              child: Text(
+                                widget.propertyData['acf']['type'],
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.blueGrey[600],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 20.0),
+              decoration: BoxDecoration(
+                color: const Color(0xff6C54FF),
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(150)),
+              ),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    child: Text(
+                      'Intrested in this?',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
                     ),
                   ),
-                )
-                .toList(),
-          ),
-        ],
-      )),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.all(10),
+                        height: 40,
+                        width: 250,
+                        child: TextField(
+                          maxLines: 1,
+                          decoration: InputDecoration(
+                            labelText: 'Name',
+                            filled: true,
+                            fillColor: Colors.white,
+                            prefixIcon: Icon(
+                              FontAwesomeIcons.userAlt,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.all(10),
+                        height: 40,
+                        width: 250,
+                        child: TextField(
+                          maxLines: 1,
+                          decoration: InputDecoration(
+                            labelText: 'Email',
+                            filled: true,
+                            fillColor: Colors.white,
+                            prefixIcon: Icon(
+                              FontAwesomeIcons.solidEnvelope,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.all(10),
+                        height: 40,
+                        width: 250,
+                        child: TextField(
+                          maxLines: 1,
+                          decoration: InputDecoration(
+                            labelText: 'Phone',
+                            filled: true,
+                            fillColor: Colors.white,
+                            prefixIcon: Icon(
+                              FontAwesomeIcons.phoneAlt,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.all(10),
+                        height: 40,
+                        width: 250,
+                        child: TextField(
+                          maxLines: 1,
+                          decoration: InputDecoration(
+                            labelText: 'Message',
+                            filled: true,
+                            fillColor: Colors.white,
+                            prefixIcon: Icon(
+                              FontAwesomeIcons.envelopeOpenText,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        child: FlatButton(
+                          onPressed: () {
+                            print('supp');
+                          },
+                          child: Text(
+                            'SEND',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          color: const Color(0xff2A3143),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
